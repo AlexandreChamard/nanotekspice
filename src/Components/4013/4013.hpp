c5b6d34088002f33d5ed79fe4298508102055319
Alexandre Chamard-bois <alexandre.chamard-bois@epitech.eu>
2018-03-02 19:28:55 +0100

    test passing D_LATCHER

--- src/Components/4013/4013.hpp
+++ src/Components/4013/4013.hpp
@@ -52,7 +52,7 @@ namespace nts {
 			{ /* P1 -> _outputs[0] */
 				PIN_OUTPUT,
 				computeFactory(0, [&](){
-				if ((!COMPUTE_REF(_inputs[0])) &
+				if (((!COMPUTE_REF(_inputs[0])) | ((!COMPUTE_REF(_inputs[0])) & (!COMPUTE_REF(_inputs[2])))) &
 					(!COMPUTE_REF(_inputs[3])) &
 					(!COMPUTE_REF(_inputs[1])))
 					return _outputs[0].state;
@@ -66,7 +66,7 @@ namespace nts {
 			{ /* P2 -> _outputs[1] */
 				PIN_OUTPUT,
 				computeFactory(1, [&](){
-				if ((!COMPUTE_REF(_inputs[0])) &
+				if (((!COMPUTE_REF(_inputs[0])) | ((!COMPUTE_REF(_inputs[0])) & (!COMPUTE_REF(_inputs[2])))) &
 					(!COMPUTE_REF(_inputs[3])) &
 					(!COMPUTE_REF(_inputs[1])))
 					return _outputs[1].state;
@@ -125,7 +125,7 @@ namespace nts {
 			{ /* P12 -> _outputs[2] */
 				PIN_OUTPUT,
 				computeFactory(2, [&](){
-				if ((!COMPUTE_REF(_inputs[7])) &
+				if (((!COMPUTE_REF(_inputs[7])) | ((!COMPUTE_REF(_inputs[7])) & (!COMPUTE_REF(_inputs[5])))) &
 					(!COMPUTE_REF(_inputs[6])) &
 					(!COMPUTE_REF(_inputs[4])))
 					return _outputs[2].state;
@@ -139,7 +139,7 @@ namespace nts {
 			{ /* P13 -> _outputs[3] */
 				PIN_OUTPUT,
 				computeFactory(3, [&](){
-				if ((!COMPUTE_REF(_inputs[7])) &
+				if (((!COMPUTE_REF(_inputs[7])) | ((!COMPUTE_REF(_inputs[7])) & (!COMPUTE_REF(_inputs[5])))) &
 					(!COMPUTE_REF(_inputs[6])) &
 					(!COMPUTE_REF(_inputs[4])))
 					return _outputs[3].state;
