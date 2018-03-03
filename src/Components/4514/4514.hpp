83ddefc62257e5865f325c1570294b2f43bb420a
Alexandre Chamard-bois <alexandre.chamard-bois@epitech.eu>
2018-03-03 22:13:37 +0100

    try patch 4514

--- src/Components/4514/4514.hpp
+++ src/Components/4514/4514.hpp
@@ -25,7 +25,7 @@ namespace nts {
 
 		std::string _id;
 		std::array<Output, 16> _outputs;
-		std::array<Input, 4> _inputs;
+		std::array<Input, 6> _inputs;
 
 		linkPin_t linkerFactory(std::size_t p)
 		{
@@ -38,6 +38,9 @@ namespace nts {
 		computePin_t computeFactory(std::size_t p, computePin_t comp)
 		{
 			return [&, p, comp](){
+				if (COMPUTE_REF(_inputs[5]) == TRUE) {
+					return FALSE;
+				}
 				if (_outputs[p].cycle == cycle_g) {
 					return _outputs[p].state;
 				}
@@ -48,24 +51,27 @@ namespace nts {
 
 		static const std::size_t _nbPins = 24;
 		const Ref _pinsRef[_nbPins] = {
-			{ /* P1 */
-				PIN_UNUSED,
-				nullptr,
-				nullptr
-			},
-			{ /* P2 -> _inputs[0] */
+			{ /* P1 _inputs[0] */
 				PIN_INPUT,
 				[&]() {return COMPUTE_REF(_inputs[0]);},
 				linkerFactory(0)
 			},
-			{ /* P3 -> _inputs[1] */
+			{ /* P2 -> _inputs[1] */
 				PIN_INPUT,
 				[&]() {return COMPUTE_REF(_inputs[1]);},
 				linkerFactory(1)
 			},
+			{ /* P3 -> _inputs[2] */
+				PIN_INPUT,
+				[&]() {return COMPUTE_REF(_inputs[2]);},
+				linkerFactory(2)
+			},
 			{ /* P4 -> _outputs[0] */
 				PIN_OUTPUT,
 				computeFactory(0, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[0].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((COMPUTE_REF(_inputs[0])) &
 						(COMPUTE_REF(_inputs[1])) &
@@ -77,6 +83,9 @@ namespace nts {
 			{ /* P5 -> _outputs[1] */
 				PIN_OUTPUT,
 				computeFactory(1, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[1].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((!COMPUTE_REF(_inputs[0])) &
 						(COMPUTE_REF(_inputs[1])) &
@@ -88,6 +97,9 @@ namespace nts {
 			{ /* P6 -> _outputs[2] */
 				PIN_OUTPUT,
 				computeFactory(2, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[2].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((COMPUTE_REF(_inputs[0])) &
 						(!COMPUTE_REF(_inputs[1])) &
@@ -99,6 +111,9 @@ namespace nts {
 			{ /* P7 -> _outputs[3] */
 				PIN_OUTPUT,
 				computeFactory(3, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[3].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((!COMPUTE_REF(_inputs[0])) &
 						(COMPUTE_REF(_inputs[1])) &
@@ -110,6 +125,9 @@ namespace nts {
 			{ /* P8 -> _outputs[4] */
 				PIN_OUTPUT,
 				computeFactory(4, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[4].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((COMPUTE_REF(_inputs[0])) &
 						(COMPUTE_REF(_inputs[1])) &
@@ -121,6 +139,9 @@ namespace nts {
 			{ /* P9 -> _outputs[5] */
 				PIN_OUTPUT,
 				computeFactory(5, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[5].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((COMPUTE_REF(_inputs[0])) &
 						(!COMPUTE_REF(_inputs[1])) &
@@ -132,6 +153,9 @@ namespace nts {
 			{ /* P10 -> _outputs[6] */
 				PIN_OUTPUT,
 				computeFactory(6, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[6].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((!COMPUTE_REF(_inputs[0])) &
 						(COMPUTE_REF(_inputs[1])) &
@@ -143,6 +167,9 @@ namespace nts {
 			{ /* P11 -> _outputs[7] */
 				PIN_OUTPUT,
 				computeFactory(7, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[7].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((!COMPUTE_REF(_inputs[0])) &
 						(!COMPUTE_REF(_inputs[1])) &
@@ -159,6 +186,9 @@ namespace nts {
 			{ /* P13 -> _outputs[8] */
 				PIN_OUTPUT,
 				computeFactory(8, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[8].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((COMPUTE_REF(_inputs[0])) &
 						(!COMPUTE_REF(_inputs[1])) &
@@ -170,6 +200,9 @@ namespace nts {
 			{ /* P14 -> _outputs[9] */
 				PIN_OUTPUT,
 				computeFactory(9, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[9].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((!COMPUTE_REF(_inputs[0])) &
 						(!COMPUTE_REF(_inputs[1])) &
@@ -181,6 +214,9 @@ namespace nts {
 			{ /* P15 -> _outputs[10] */
 				PIN_OUTPUT,
 				computeFactory(10, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[10].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((COMPUTE_REF(_inputs[0])) &
 						(COMPUTE_REF(_inputs[1])) &
@@ -192,6 +228,9 @@ namespace nts {
 			{ /* P16 -> _outputs[11] */
 				PIN_OUTPUT,
 				computeFactory(11, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[11].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((!COMPUTE_REF(_inputs[0])) &
 						(COMPUTE_REF(_inputs[1])) &
@@ -203,6 +242,9 @@ namespace nts {
 			{ /* P17 -> _outputs[12] */
 				PIN_OUTPUT,
 				computeFactory(12, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[12].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((COMPUTE_REF(_inputs[0])) &
 						(!COMPUTE_REF(_inputs[1])) &
@@ -214,6 +256,9 @@ namespace nts {
 			{ /* P18 -> _outputs[13] */
 				PIN_OUTPUT,
 				computeFactory(13, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[13].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((!COMPUTE_REF(_inputs[0])) &
 						(!COMPUTE_REF(_inputs[1])) &
@@ -225,6 +270,9 @@ namespace nts {
 			{ /* P19 -> _outputs[14] */
 				PIN_OUTPUT,
 				computeFactory(14, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[14].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((COMPUTE_REF(_inputs[0])) &
 						(COMPUTE_REF(_inputs[1])) &
@@ -235,7 +283,10 @@ namespace nts {
 			},
 			{ /* P20 -> _outputs[15] */
 				PIN_OUTPUT,
-				computeFactory(1, [&](){
+				computeFactory(15, [&](){
+					if (COMPUTE_REF(_inputs[0]) == FALSE) {
+						return _outputs[15].state;
+					}
 					return	(!COMPUTE_REF(_inputs[4])) &
 						((!COMPUTE_REF(_inputs[0])) &
 						(COMPUTE_REF(_inputs[1])) &
@@ -244,20 +295,20 @@ namespace nts {
 				}),
 				nullptr
 			},
-			{ /* P21 -> _inputs[2] */
-				PIN_INPUT,
-				[&]() {return COMPUTE_REF(_inputs[2]);},
-				linkerFactory(2)
-			},
-			{ /* P22 -> _inputs[3] */
+			{ /* P21 -> _inputs[3] */
 				PIN_INPUT,
 				[&]() {return COMPUTE_REF(_inputs[3]);},
 				linkerFactory(3)
 			},
-			{ /* P23 -> _inputs[4] */
+			{ /* P22 -> _inputs[4] */
 				PIN_INPUT,
 				[&]() {return COMPUTE_REF(_inputs[4]);},
 				linkerFactory(4)
+			},
+			{ /* P23 -> _inputs[5] */
+				PIN_INPUT,
+				[&]() {return COMPUTE_REF(_inputs[5]);},
+				linkerFactory(5)
 			},
 			{ /* P24 */
 				PIN_UNUSED,
