9c9c926e3070f0cff8b747f5acc86ad4c993b31a
Alexandre Chamard-bois <alexandre.chamard-bois@epitech.eu>
2018-03-03 21:26:33 +0100

    try patch 4017

--- src/Components/4017/4017.hpp
+++ src/Components/4017/4017.hpp
@@ -24,7 +24,7 @@ namespace nts {
 		static unsigned int id;
 
 		std::string _id;
-		std::size_t _count = 1;
+		std::size_t _count = 0;
 		std::size_t _cycle = 0;
 		std::array<Output, 11> _outputs;
 		std::array<Input, 3> _inputs;
@@ -43,7 +43,7 @@ namespace nts {
 				if (_cycle != cycle_g) {
 					_cycle = cycle_g;
 					if ((COMPUTE_REF(_inputs[0]) != TRUE &&
-					COMPUTE_REF(_inputs[1]) == TRUE))
+					COMPUTE_REF(_inputs[1]) == TRUE) || _count == 0)
 						_count++;
 					if (COMPUTE_REF(_inputs[2]) == TRUE)
 						_count = 0;
